ddeff09ef20886d6620599ef1f5fcf5f045109a8
Daniel Šebesta <dast@duck.com>
2023-11-07 08:46:22 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,3 +1,15 @@
 <?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Midaval</title>
+</head>
+<body>
+    
+</body>
+</html>';
+
 echo "Hello TdA";
 ?>
