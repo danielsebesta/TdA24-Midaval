ae30009575020001ebb89f58af6e050860444071
Daniel Šebesta <dast@duck.com>
2023-11-13 10:14:59 +0100

    Update index.php

--- php/lecturer/index.php
+++ php/lecturer/index.php
@@ -8,5 +8,18 @@
 </head>
 <body>
 	Lecturer
+	Petra
+	Plachá
+	Mgr.
+	
+	Swil
+	MBA
+	Brno
+Aktivní studentka / Předsedkyně spolku / Projektová manažerka
+	Momentálně studuji Pdf MUNI a FF MUNI v Brně.
+	Dobrovolnictví
+	Prezentační dovednosti
+	Fundraising pro neziskové studentské projekty
+	<img src="https://tourdeapp.cz/storage/images/2023_02_25/412ff296a291f021bbb6de10e8d0b94863fa89308843b/big.png.webp">
 </body>
 </html>
