4cbb5156af4fda39c728bf4aae533b59306ce48a
Daniel Šebesta <dast@duck.com>
2024-01-04 17:05:53 +0100

    Update testdb.php 2

--- php/testdb.php
+++ php/testdb.php
@@ -5,24 +5,11 @@ $username = "databaze"; // Uživatelské jméno pro přihlášení k databázi
 $password = getenv('DB_PASSWORD'); // Načtení hesla z repozitářového tajemství v GitHubu
 $dbname = "lecturerdb"; // Jméno databáze
 
-// Vytvoření spojení
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Kontrola spojení
-if ($conn->connect_error) {
-    die("Spojení se nezdařilo: " . $conn->connect_error);
+/try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    echo "Connected successfully!";
+    $conn->close();
+} catch(mysqli_sql_exception $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-
-// Dotaz pro vytvořenou tabulku
-$sql = "SELECT * FROM lecturers";
-$result = $conn->query($sql);
-
-if ($result === false) {
-    echo "Chyba při provádění dotazu: " . $conn->error;
-} else {
-    echo "Spojení k databázi bylo úspěšné! Tabulka 'lecturers' byla vytvořena.";
-}
-
-// Uzavření spojení
-$conn->close();
 ?>
