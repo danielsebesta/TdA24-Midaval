9c72b503f8e4bb0cc2c6f911ef0bf48fb6b1af4c
Daniel Šebesta <dast@duck.com>
2024-01-04 14:45:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,11 +9,6 @@ echo "CREATE DATABASE IF NOT EXISTS db" | mysql
 # Import the database
 mysql db < /app/database.sql
 
-
-chmod 666 /php/api/lecturers/database.db
-chmod -R 664 /php/api/lecturers
-chown -R www-data:www-data /php/api/lecturers
-
 # Start Apache in the foreground
 apache2ctl -D FOREGROUND
 
