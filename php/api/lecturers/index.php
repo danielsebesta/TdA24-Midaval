ce2efe6387ec76e109cfa6abd03b616204587821
Daniel Šebesta <dast@duck.com>
2024-01-11 18:04:28 +0100

    making changes

--- php/api/lecturers/index.php
+++ php/api/lecturers/index.php
@@ -228,7 +228,7 @@ try {
 		exit();
 	}
 
-	$db->close();
+	$conn->close();
 } catch (Exception $e) {
 	http_response_code(500);
 	echo json_encode(["error" => $e->getMessage()], JSON_FORCE_OBJECT);
